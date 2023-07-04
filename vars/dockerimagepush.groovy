6bfd39c411a54a3822203468176b3ce15327827f
fadedstarboy <ishaanraghavjames@gmail.com>
2023-07-04 20:42:07 +0530

    Added dockerpush

---
+++ vars/dockerimagepush.groovy
@@ -0,0 +1,12 @@
+def call(String project,String ImageTag, String hubUser){
+    withCredentials([usernamePassword(
+        credentialID: "dockerhub"
+        usernameVariable: "USER"
+        passwordVariable: "PASS"
+    )])
+    {
+        sh "docker login -u '$USER' -p '$PASS'"
+    }
+    sh "docker image push ${hubUser}/${project}:${ImageTag}"
+    sh "docker image push ${hubUser}/${project}:latest"
+}
